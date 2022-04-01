5461916775d0ce2ab1afde291d0fb2d7bab8ad0a
Vali <valiruziboyev212@gmail.com>
2022-04-01 10:10:09 +0500

    route structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 import Main from './components/Main';
+import {MemoryRouter as Router, Switch, Route} from 'react-router-dom'
+import History from './components/History';
 
 function App() {
   return (
-    <div className="App">
-        <Main />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path='/'>
+            <Main />
+          </Route>
+          <Route path='/history'>
+            <History />
+          </Route>
+        </Switch>
+
+      </div>
+    </Router>
   );
 }
 
