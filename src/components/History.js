0af69ae0f5c7f5ddf4152045683fd63619c0894d
Vali <valiruziboyev212@gmail.com>
2022-04-12 13:26:16 +0500

    last modified time functonality

--- src/components/History.js
+++ src/components/History.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import Password from "./Password";
 import { PasswordContext } from "../contexts/PasswordContext";
-import { useContext, useRef, useState } from "react";
+import { useContext, useState } from "react";
 
 const History = () => {
     const {password, dispatch} = useContext(PasswordContext)
@@ -40,17 +40,18 @@ const History = () => {
         })
         navigator.clipboard.writeText(passList.join(', '))
     }
+    
     return ( 
-    <div className="main">
+    <div className="main extpad">
         <nav className="back">
             <Link  to='/'>
                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>
             </Link>
         </nav>
         <main>
-        {password.map(pass=>{
+        {password.sort((a,b)=>b.date.toString().localeCompare(a.date.toString())).map(pass=>{
             return(
-                <Password key={pass.id} id={pass.id} password={pass.password} dispatch={dispatch} checked={setChecked}/>
+                <Password key={pass.id} id={pass.id} password={pass.password} date={pass.date} dispatch={dispatch} checked={setChecked}/>
             )
         })}
         </main>
